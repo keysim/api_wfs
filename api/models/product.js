129fe468a4137b13b513559cfca6285e386b6769
menard_s <simon.menard@epitech.eu>
2016-12-13 11:00:59 +0800

    upload to post product

--- api/models/product.js
+++ api/models/product.js
@@ -42,6 +42,7 @@ module.exports = {
     },
 
 	postProduct : function(req, res) {
+		console.log(req.body);
 		var data = utils.mask_obj(req.body, config.model.product);
 		data.seller = req.user._id;
 		console.log(data);
