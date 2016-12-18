20585fc16b5d9b2cb7dc99f9c57dc2c40dcd6911
menard_s <simon.menard@epitech.eu>
2016-12-19 00:40:16 +0800

    upload to post product

--- api/config.js
+++ api/config.js
@@ -2,7 +2,7 @@ var config = {
     secret: 'api_wfs',
     port : 4242,
     url : "http://lewogona.com:" + 4242,
-    db : {url:'mongodb:///lewogona.com:27017/wfs', port:27017, name:'wfs'}, // launch "mongod" then take port and host printed in the command line.
+    db : {url:'mongodb://lewogona.com:27017/wfs', port:27017, name:'wfs'}, // launch "mongod" then take port and host printed in the command line.
     model:{
         user:{
             login: {type: String, required: true},
